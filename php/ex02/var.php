761eb0151c6dd0e6046274e3849475f7116318e8
Victor Aguiar <victoraguiarrosa397@gmail.com>
2023-01-03 19:16:10 -0300

    algunos

---
+++ php/ex02/var.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="IE-edge">
+		<title>Var</title>
+	</head>
+	<body>
+		<?php
+			$nome = "Roza";
+			$idade = 17;
+			$dinheiro = 20.5;
+			$namorando = true;
+			echo "$nome de $idade anos tem $dinheiro reais.";
+		?>
+	</body>
+</html>
